81c421c5b7a64bd7054b89f274ae085c324ef9d9
Luke <gianluca.suriani@outlook.it>
2021-05-08 10:12:53 +0200

    CSS fixes and code optimization

--- src/index.js
+++ src/index.js
@@ -8,24 +8,18 @@ chrome.runtime.onInstalled.addListener(function() {
 
 function getOrdinalNum(n) {
     return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
-  }
+}
 
   // what the fuck is this
-  function tabNameTime() {
-            	
+function tabNameTime() {
+    var Symbol;
     var today = new Date();
-    
-    if(today.getMinutes() < 10){
-     var time = today.getHours() + ":0" + today.getMinutes()
-    } else {
-     var time = today.getHours() + ":" + today.getMinutes()
-    }
+    var time = today.getHours() + ":" + String(today.getMinutes()).padStart(2, "0");
     console.log(time)
 
+    // Month - Day - Year
     const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-
     var fulldate = monthNames[today.getMonth()] + " " +  getOrdinalNum(today.getDay()) + ", " + today.getFullYear()
-    
     
     document.title = "New Tab | " + time;
     document.getElementById("clock").innerText = time;
@@ -37,29 +31,32 @@ function getOrdinalNum(n) {
 tabNameTime()
 
 
-
+// there must be a better way of doing this, will check later...
 $('input[type="text"]').on('focus', function() {
     document.getElementById("bg").className = "background-hvr"
 });
 
 $('input[type="text"]').on('focusout', function() {
-    document.getElementById("bg").className = "background-hvr"
     document.getElementById("bg").className = "background"
 });
 
-
-
-
+// Focus on search bar whenever a key gets pressed
 document.onkeypress = function (e) {
     document.getElementById("search").focus();
+    if (e.key == "Escape") {
+        console.log("hi")
+        document.getElementById("Search").blur();
+    }
 };
 
-function browse(){
-    query = document.getElementById("search").innerText;
-    if(query.length <= 0){
-        return alert("[DEBUG] the query seems to be empty!")
-    }
-    window.open("http://www.google.com/search?q=","_self")
-}
 
-
+// it doesn't work, nice
+$(function(){
+    var bgimage = new Image();      
+    bgimage.src="https://source.unsplash.com/1920x1080/?landscape";       
+    $(".background").hide();
+    $(".background-hvr").hide();
+    $(bgimage).on('load',function(){
+        $(".background").css("background-image","url("+$(this).attr("src")+")").show()                  
+    });
+});
